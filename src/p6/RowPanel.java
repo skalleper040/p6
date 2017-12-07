eff425f1c6077d42a97fbe613abc5ece4682f629
Vixen666 <johannesroos87@gmail.com>
2017-12-08 00:04:42 +0100

    RowPanel

---
+++ src/p6/RowPanel.java
@@ -0,0 +1,20 @@
+package p6;
+
+import java.awt.Dimension;
+import java.awt.GridLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class RowPanel extends JPanel {
+	
+	public RowPanel (int cols, int width, int height) {
+		JTextField[] colTextFields = new JTextField[cols];
+		setPreferredSize(new Dimension(width,height));
+		setLayout(new GridLayout(1,cols,4,4));
+		for (int col=0; col<cols; col++) {
+			colTextFields[col] = new JTextField("0");
+			add(colTextFields[col]);
+		}
+	}
+}
