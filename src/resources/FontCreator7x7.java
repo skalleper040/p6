a58a2cd4a80c74013aa5292a4f92b5f6c8c5e711
Vixen666 <johannesroos87@gmail.com>
2017-12-18 20:38:44 +0100

    Added next char

--- src/resources/FontCreator7x7.java
+++ src/resources/FontCreator7x7.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -20,10 +21,14 @@ import controllers.Test1UIController;
 public class FontCreator7x7 extends JFrame implements ActionListener{
 	private ButtonArrayPanel buttonArrayPanel = new ButtonArrayPanel();
 	private JPanel mainPanel = new JPanel();
+	private JPanel botPanel = new JPanel();
 	private FontController controller;
-	private JTextField letter = new JTextField();
-	private JButton read = new JButton("Skriv till c:\\output.txt");
+	private JTextField letter = new JTextField("A");
+	private JButton read = new JButton("Save");
 	private JButton reset = new JButton("Reset");
+	private JButton next = new JButton("Save and Next");
+	private String alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖabcdefghijklmnopqrstuvwxyzåäö0123456789!#()=?.,-_";
+	
 	public FontCreator7x7() {
 		//this.setPreferredSize(new Dimension(900,900));
 		this.setLayout(new BorderLayout());
@@ -31,11 +36,15 @@ public class FontCreator7x7 extends JFrame implements ActionListener{
 		this.add (mainPanel, BorderLayout.CENTER);
 		mainPanel.add (buttonArrayPanel, BorderLayout.CENTER);
 		letter.setHorizontalAlignment(JTextField.CENTER);
-		this.add(letter, BorderLayout.SOUTH);
-		this.add(read, BorderLayout.WEST);
+		this.add(botPanel, BorderLayout.SOUTH);
+		botPanel.setLayout(new GridLayout(4,1,10,10));
+		botPanel.add(letter);
+		botPanel.add(next);
+		botPanel.add(read);
+		botPanel.add(reset);
 		read.addActionListener(this);
-		this.add(reset, BorderLayout.NORTH);
 		reset.addActionListener(this);
+		next.addActionListener(this);
 		for (int row=0; row<7; row++) {
 			for (int col=0; col<7; col++) {
 				this.buttonArrayPanel.getButton(row, col).addActionListener(this);
@@ -64,6 +73,20 @@ public class FontCreator7x7 extends JFrame implements ActionListener{
 		if(e.getSource() == this.reset) {
 			this.reset();
 			controller.reset();
+		}
+		if(e.getSource() == this.next) {
+			try {
+				this.controller.toText(this.letter.getText());
+			} catch (FileNotFoundException | UnsupportedEncodingException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			this.reset();
+			controller.reset();
+			int index = alpha.indexOf(this.letter.getText());
+			if( index < alpha.length()) {
+				this.letter.setText(""+alpha.charAt(index+1));
+			}
 		}
 		if(e.getSource() == this.read) {
 			try {
