4353bbf06b686b5407229d5d499598077567eb3c
Vixen666 <johannesroos87@gmail.com>
2018-01-08 16:39:33 +0000

    1-7fix

--- src/controllers/Test1UIController.java
+++ src/controllers/Test1UIController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 import resources.Array7;
@@ -38,11 +39,22 @@ public class Test1UIController {
 	 */
 	public void read(boolean row) {
 		if(row) {
-			bottomRow = array.getRow(test1ui.getRowNbr()-1);
-			test1ui.setBotRow(bottomRow);
+			if(1<= test1ui.getRowNbr() && test1ui.getRowNbr() <=7) {
+
+				bottomRow = array.getRow(test1ui.getRowNbr()-1);
+				test1ui.setBotRow(bottomRow);	
+			}else {
+				JOptionPane.showMessageDialog(null,"Fel");
+			}
 		}else {
-			leftColum = array.getCol(test1ui.getColNbr()-1);
-			test1ui.setLeftCol(leftColum);
+			if(1<= test1ui.getColNbr() && test1ui.getColNbr() <=7) {
+				leftColum = array.getCol(test1ui.getColNbr()-1);
+				test1ui.setLeftCol(leftColum);
+			}else {
+				JOptionPane.showMessageDialog(null,"Fel");
+
+			}
+			
 		}
 	}
 	
@@ -53,9 +65,19 @@ public class Test1UIController {
 	 */
 	public void write(boolean row) {
 		if(row) {
-			array.setRow(test1ui.getRowNbr()-1, test1ui.getBotRow());
+			if(1 <= test1ui.getRowNbr() && test1ui.getRowNbr() <=7) {
+				array.setRow(test1ui.getRowNbr()-1, test1ui.getBotRow());
+			}else {
+				JOptionPane.showMessageDialog(null,"Fel");
+			}
+			
 		}else {
-			array.setCol(test1ui.getColNbr()-1, test1ui.getLeftCol());
+			if(1 <= test1ui.getColNbr() && test1ui.getColNbr() <=7) {
+				array.setCol(test1ui.getColNbr()-1, test1ui.getLeftCol());
+			}else {
+				JOptionPane.showMessageDialog(null,"Fel");
+			}
+			
 		}
 		
 		showArray();
